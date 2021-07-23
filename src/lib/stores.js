7407e37044165af542ae1d018d18fd66305e2ede
Angellomon <angelmtzdiaz@gmail.com>
2021-07-23 17:46:47 -0500

    feat(stores): implementación de stores para formulario

---
+++ src/lib/stores.js
@@ -0,0 +1,27 @@
+import { writable } from 'svelte/store';
+
+export const stepForm = writable(0);
+
+export const currentForm = writable(1);
+
+export const toggles = writable({
+  tendencia: false,
+  frustracionOfrecimiento: false,
+  frustracionCompetencia: false
+});
+
+export const formValues = writable({
+  identificaciones: {
+    tendencia: '',
+    nuestraOferta: '',
+    competencia: ''
+  },
+  solucionInovadora: '',
+  formaDiferente: '',
+  productoNuevo: '',
+  recopilacionDatos: '',
+  descripcion: '',
+  pruebaIdea: '',
+  oportunidadVerdadera: '',
+  requiereFondeo: false
+});
