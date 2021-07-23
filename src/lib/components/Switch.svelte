f6f7e6ce0ed992da2afc7bbcda7f1539e7bbde8b
Angellomon <angelmtzdiaz@gmail.com>
2021-07-23 17:48:59 -0500

    feat(componentes): Mejoras de Switch (colores)

--- src/lib/components/Switch.svelte
+++ src/lib/components/Switch.svelte
@@ -1,65 +1,75 @@
 <script>
-	export let checked = false;
-	// export let color = "#bed74b";
+  export let checked = false;
+  // export let color = "#bed74b";
+
+  export let green = true;
 </script>
 
 <label class="switch">
-	<input type="checkbox" bind:checked />
-	<span class="slider" />
+  <input type="checkbox" bind:checked />
+  <span class={green ? 'slider green' : 'slider blue'} />
 </label>
 
 <style>
-	.switch {
-		position: relative;
-		display: inline-block;
-		width: 60px;
-		height: 34px;
-		align-self: center;
-	}
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+    align-self: center;
+  }
 
-	.switch input {
-		opacity: 0;
-		width: 0;
-		height: 0;
-	}
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-	.slider {
-		position: absolute;
-		cursor: pointer;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background-color: #ccc;
-		-webkit-transition: 0.4s;
-		transition: 0.4s;
-		border-radius: 34px;
-	}
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 34px;
+  }
 
-	.slider:before {
-		position: absolute;
-		content: '';
-		height: 26px;
-		width: 26px;
-		left: 4px;
-		bottom: 4px;
-		background-color: white;
-		-webkit-transition: 0.4s;
-		transition: 0.4s;
-		border-radius: 50%;
-	}
+  .slider:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 50%;
+  }
 
-	input:checked + .slider {
-		background-color: #bed74b;
-	}
+  input:checked + .green {
+    background-color: #bed74b;
+  }
 
-	input:checked + .slider {
-		box-shadow: 0 0 1px #bed74b;
-	}
+  input:checked + .green {
+    box-shadow: 0 0 1px #bed74b;
+  }
 
-	input:checked + .slider:before {
-		-webkit-transform: translateX(26px);
-		-ms-transform: translateX(26px);
-		transform: translateX(26px);
-	}
+  input:checked + .blue {
+    background-color: #75c7ae;
+  }
+
+  input:checked + .blue {
+    box-shadow: 0 0 1px #75c7ae;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 </style>
