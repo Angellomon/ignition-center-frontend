223456ab36b5de210a0d947501bf245dabf90b79
Angellomon <angelmtzdiaz@gmail.com>
2021-07-23 17:48:20 -0500

    feat(routes): Implementación de enrutamiento

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
-import Principal from "./lib/pages/Principal.svelte"
-import Mensaje from "./lib/pages/Mensaje.svelte"
+import Principal from './lib/pages/Principal.svelte';
+import Mensaje from './lib/pages/Mensaje.svelte';
+import Formulario from './lib/pages/Formulario.svelte';
+import Manifesto from './lib/pages/Manifesto.svelte';
 
 export const routes = {
-  "/": Principal,
-  "/msg": Mensaje
-}
+  '/': Principal,
+  '/msg': Mensaje,
+  '/formulario': Formulario,
+  '/manifesto': Manifesto
+};
