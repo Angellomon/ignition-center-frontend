3a46108a5c0a4836105a1c5634de2de97cec56a6
Angellomon <angelmtzdiaz@gmail.com>
2021-07-29 23:10:51 -0500

    feat(redirect): Implementacion a redirect apara sitrio interno de cliente

--- src/lib/pages/Principal.svelte
+++ src/lib/pages/Principal.svelte
@@ -1,63 +1,72 @@
 <script>
-	import { fade } from 'svelte/transition';
-	import { push } from 'svelte-spa-router';
-	import Switch from '../components/Switch.svelte';
-	import Titulo from '../components/Titulo.svelte';
+  import { fade } from 'svelte/transition';
+  import Switch from '../components/Switch.svelte';
+  import Titulo from '../components/Titulo.svelte';
 
-	let on = false;
-	let timeout;
+  let on = false;
+  let redirect = false;
+  let timeout;
+  let timeout2;
 
-	$: if (on) {
-		timeout = setTimeout(() => {
-			push('/msg');
-		}, 3000);
-	} else {
-		clearTimeout(timeout);
-	}
+  $: if (on) {
+    timeout = setTimeout(() => {
+      redirect = true;
+    }, 3000);
+  } else {
+    clearTimeout(timeout);
+  }
+  $: if (redirect) {
+    timeout2 = setTimeout(() => {
+      window.location.href =
+        'https://collaboration.merck.com/sites/SoFMexico/SitePages/ignitionCenter.aspx';
+    }, 400);
+  }
 </script>
 
 <section class:on>
-	<div class="container">
-		<Titulo />
-		<Switch bind:checked={on} />
-		{#if on}
-			<div class="mensaje">
-				<p transition:fade>Powered by Subsidiary</p>
-				<p transition:fade>of the Future</p>
-			</div>
-		{/if}
-	</div>
+  {#if !redirect}
+    <div class="container" transition:fade>
+      <Titulo />
+      <Switch bind:checked={on} />
+      {#if on}
+        <div class="mensaje">
+          <p transition:fade>Powered by Subsidiary</p>
+          <p transition:fade>of the Future</p>
+        </div>
+      {/if}
+    </div>
+  {/if}
 </section>
 
 <style>
-	p {
-		color: #75c7ae;
-		text-align: center;
-		margin: 0;
-		font-weight: bold;
-	}
-	div.mensaje {
-		margin-top: 5em;
-	}
-	div.container {
-		margin: 5em;
-		padding: 5em;
-		display: flex;
-		flex-direction: column;
-		align-content: center;
-	}
-	section {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		background-color: #13213d;
-		min-width: 100vw;
-		min-height: 100vh;
-	}
-	.on {
-		background-color: white;
-		-webkit-transition: background-color 500ms linear;
-		-ms-transition: background-color 500ms linear;
-		transition: background-color 500ms linear;
-	}
+  p {
+    color: #75c7ae;
+    text-align: center;
+    margin: 0;
+    font-weight: bold;
+  }
+  div.mensaje {
+    margin-top: 5em;
+  }
+  div.container {
+    margin: 5em;
+    padding: 5em;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+  }
+  section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #13213d;
+    min-width: 100vw;
+    min-height: 100vh;
+  }
+  .on {
+    background-color: white;
+    -webkit-transition: background-color 500ms linear;
+    -ms-transition: background-color 500ms linear;
+    transition: background-color 500ms linear;
+  }
 </style>
