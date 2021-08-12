ed43c3d76bad63d4605d2f87b6e1cffce71f3364
Angellomon <angelmtzdiaz@gmail.com>
2021-08-12 18:01:15 -0500

    feat(info): Implementación de cursor

---
+++ src/lib/components/Cursor.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { spring } from 'svelte/motion';
+  import { seccion } from '../stores';
+
+  export let x = 0;
+  export let y = 0;
+
+  const size = spring(20);
+</script>
+
+<div
+  on:click={() => console.log($seccion)}
+  on:mousedown={() => size.set(35)}
+  on:mouseup={() => size.set(20)}
+  class="circle"
+  style={`left: ${x - $size / 2}px; top: ${y - $size / 2}px; height: ${$size}px; width: ${$size}px`}
+/>
+
+<style>
+  .circle {
+    background-color: #ff3e00;
+    border-radius: 50%;
+    display: inline-block;
+    position: absolute;
+  }
+</style>
