74c1b377fbb419f09d6f6069729239cd95a34539
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:22:31 -0500

    feat(secciones): implementación para sección agile

---
+++ src/lib/components/info/Agile.svelte
@@ -0,0 +1,23 @@
+<script>
+  import GrafoAgile from '../grafo/GrafoAgile.svelte';
+  import ColWrap from './ColWrap.svelte';
+
+  import { seccion } from '../../stores';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<ColWrap>
+  <h3>¿qué es agile?</h3>
+  <GrafoAgile bind:coords on:enter on:leave animation={$seccion === 'ver_iniciativas'} />
+</ColWrap>
+
+<style>
+  h3 {
+    text-align: center;
+    color: white;
+  }
+</style>
