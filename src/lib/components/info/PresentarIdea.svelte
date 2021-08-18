ea4a4d7cc9e86fe6756c7cdc4998ff18569215c7
Angellomon <angelmtzdiaz@gmail.com>
2021-08-17 22:26:24 -0500

    feat(secciones): correcciones en secciones presentar idea y que es IG

--- src/lib/components/info/PresentarIdea.svelte
+++ src/lib/components/info/PresentarIdea.svelte
@@ -2,6 +2,8 @@
   import GrafoPresentar from '../grafo/GrafoPresentar.svelte';
   import ColWrap from './ColWrap.svelte';
 
+  import { seccion } from '../../stores';
+
   export let coords = {
     x: 0,
     y: 0
@@ -10,11 +12,12 @@
 
 <ColWrap>
   <h3>quiero presentar mi idea</h3>
-  <GrafoPresentar bind:coords on:enter on:leave />
+  <GrafoPresentar bind:coords on:enter on:leave animation={$seccion === 'presentar_idea'} />
 </ColWrap>
 
 <style>
   h3 {
     text-align: center;
+    color: #13213d;
   }
 </style>
--- src/lib/components/info/QueEsIG.svelte
+++ src/lib/components/info/QueEsIG.svelte
@@ -1,6 +1,7 @@
 <script>
   import ColWrap from './ColWrap.svelte';
   import GrafoQueEs from '../grafo/GrafoQueEs.svelte';
+  import { seccion } from '../../stores';
 
   export let coords = {
     x: 0,
@@ -10,7 +11,7 @@
 
 <ColWrap>
   <h3>¿qué es ignition center?</h3>
-  <GrafoQueEs bind:coords on:enter on:leave />
+  <GrafoQueEs bind:coords on:enter on:leave animation={$seccion === 'que_es'} />
 </ColWrap>
 
 <style>
