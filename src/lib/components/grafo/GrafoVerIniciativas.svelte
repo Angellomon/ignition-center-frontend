1a6189cfb9fa407ac74349666f0ae61f569f6082
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:55:34 -0500

    chore(animaciones/estilos): cambios a estructura y limpieza

--- src/lib/components/grafo/GrafoVerIniciativas.svelte
+++ src/lib/components/grafo/GrafoVerIniciativas.svelte
@@ -1,99 +1,10 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
-
   import Nodo from './Nodo.svelte';
-  import { cursorPosition } from '../../stores';
-  import { offset, DURACION_ANIMACION } from '../../utils';
 
   export let coords = {
     x: 0,
     y: 0
   };
-
-  export let animation = false;
-
-  let posicionInicial;
-
-  let interval;
-
-  let grafo;
-
-  const offssetsPosiciones = [
-    {
-      x: 0,
-      y: 0
-    },
-    {
-      x: 0,
-      y: 50
-    }
-  ];
-
-  const INICIO = 0,
-    FIN = offssetsPosiciones.length - 1,
-    TOP_OFFSET = 20,
-    LEFT_OFFSET = 23;
-
-  let etapa = INICIO;
-
-  const actualizarPosicion = () => {
-    if (!animation) {
-      // etapa = INICIO;
-      return;
-    }
-
-    cursorPosition.set({
-      x: posicionInicial.left + offssetsPosiciones[etapa].x,
-      y: posicionInicial.top + offssetsPosiciones[etapa].y
-    });
-
-    if (etapa === FIN) {
-      etapa = INICIO;
-    } else {
-      etapa += 1;
-    }
-  };
-
-  const siguienteEtapa = () => {
-    if (!animation) {
-      etapa = INICIO;
-      return;
-    }
-
-    actualizarPosicion();
-  };
-
-  onMount(() => {
-    grafo = document.querySelector('.grafo-ver-iniciativas');
-    if (!grafo) return;
-
-    const pos = offset(grafo);
-
-    pos.left += LEFT_OFFSET;
-    pos.top += TOP_OFFSET;
-
-    // cursorPosition.set({
-    //   x: pos.left,
-    //   y: pos.top
-    // });
-    posicionInicial = pos;
-
-    // interval = setInterval(() => {
-    //   siguienteEtapa();
-    // }, 1500);
-    console.log('listo', animation);
-  });
-  onDestroy(() => clearInterval(interval));
-
-  $: if (!animation) {
-    clearInterval(interval);
-  } else {
-    actualizarPosicion();
-
-    interval = setInterval(() => {
-      actualizarPosicion();
-    }, DURACION_ANIMACION);
-  }
 </script>
 
 <div class="grafo-ver-iniciativas">
