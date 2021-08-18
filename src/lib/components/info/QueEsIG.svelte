02c972dbf0e5d1aa9b675b249c6d05967e4db372
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:45:20 -0500

    feat(animaciones): implementación de animación para sección Que es IG

--- src/lib/components/info/QueEsIG.svelte
+++ src/lib/components/info/QueEsIG.svelte
@@ -2,6 +2,7 @@
   import ColWrap from './ColWrap.svelte';
   import GrafoQueEs from '../grafo/GrafoQueEs.svelte';
   import { seccion } from '../../stores';
+  import WrapperAnimacion from '../grafo/WrapperAnimacion.svelte';
 
   export let coords = {
     x: 0,
@@ -11,7 +12,22 @@
 
 <ColWrap>
   <h3>¿qué es ignition center?</h3>
-  <GrafoQueEs bind:coords on:enter on:leave animation={$seccion === 'que_es'} />
+  <WrapperAnimacion
+    grafoClass=".grafo-que-es"
+    animation={$seccion === 'que_es'}
+    offssetsPosiciones={[
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: 0,
+        y: 50
+      }
+    ]}
+  >
+    <GrafoQueEs bind:coords on:enter on:leave />
+  </WrapperAnimacion>
 </ColWrap>
 
 <style>
