355ecaccad0969f76f15206db62af58498f1ffcd
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:15:42 -0500

    feat(seccion): implementación de sección y grafo de scrum

---
+++ src/lib/components/grafo/GrafoScrum.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Nodo from './Nodo.svelte';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<div class="grafo-ver-iniciativas">
+  <span class="nodo-1">
+    <Nodo bind:pos={coords} on:enter on:leave darkblue />
+  </span>
+  <div class="arista" />
+  <div class="nodo-2">
+    <Nodo bind:pos={coords} on:enter on:leave darkblue />
+  </div>
+</div>
+
+<style>
+  .nodo-1 {
+    margin-left: 70px;
+    margin-top: -24px;
+  }
+  .nodo-2 {
+    margin-left: 0px;
+    margin-top: -24px;
+    z-index: -99;
+  }
+  .arista {
+    width: 2px;
+    background-color: #13213d;
+    height: 2px;
+    width: 70px;
+    margin-top: -24px;
+    margin-bottom: -20px;
+    margin-left: 25px;
+  }
+  .grafo-ver-iniciativas {
+    margin: 50px;
+    position: absolute;
+  }
+</style>
