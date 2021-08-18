6e5419fb610b2327e2503772b97fb66373132ee6
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:21:05 -0500

    feat(secciones): implementación de sección para design thinking

---
+++ src/lib/components/info/DesignThinking.svelte
@@ -0,0 +1,24 @@
+<script>
+  import GrafoAgile from '../grafo/GrafoAgile.svelte';
+  import ColWrap from './ColWrap.svelte';
+
+  import { seccion } from '../../stores';
+  import GrafoDesignThinking from '../grafo/GrafoDesignThinking.svelte';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<ColWrap>
+  <h3>¿qué es design thinking?</h3>
+  <GrafoDesignThinking bind:coords on:enter on:leave animation={$seccion === 'ver_iniciativas'} />
+</ColWrap>
+
+<style>
+  h3 {
+    text-align: center;
+    color: white;
+  }
+</style>
