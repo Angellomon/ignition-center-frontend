acacb3f8b9b90b5c105e06247145d2862a24e2ac
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:54:41 -0500

    chore(animaciones/estilos): cambios a estructura y limpieza

--- src/lib/components/grafo/GrafoConoceElProceso.svelte
+++ src/lib/components/grafo/GrafoConoceElProceso.svelte
@@ -7,42 +7,47 @@
   };
 </script>
 
-<div class="grafo">
+<div class="grafo-conoce-el-proceso">
   <span>
+    <div class="nodo-1">
+      <Nodo bind:pos={coords} on:enter on:leave />
+    </div>
+    <div class="arista-1" />
+    <div class="nodo-2">
+      <Nodo bind:pos={coords} on:enter on:leave />
+    </div>
+  </span>
+  <div class="arista-2" />
+  <div class="nodo-3">
     <Nodo bind:pos={coords} on:enter on:leave />
-    <div class="arista" />
-    <div class="offset-left">
-      <Nodo bind:pos={coords} on:enter on:leave />
-    </div>
-  </span>
-  <br />
-  <span>
-    <div class="offset-top">
-      <Nodo bind:pos={coords} on:enter on:leave />
-    </div>
-    <div class="arista-2" />
-  </span>
+  </div>
 </div>
 
 <style>
   span {
     display: inline;
   }
-  .offset-left {
+
+  .nodo-1 {
+    margin-top: -15px;
+    margin-left: 0px;
+  }
+  .nodo-2 {
     margin-left: 70px;
     margin-top: -24px;
-    z-index: -99;
   }
-  .offset-top {
-    margin-top: -25px;
-    z-index: -99;
+
+  .nodo-3 {
+    margin-left: 0px;
+    margin-top: -3px;
   }
-  .arista {
+
+  .arista-1 {
     width: 2px;
     background-color: white;
     height: 2px;
     width: 70px;
-    margin-top: -24px;
+    margin-top: -23px;
     margin-bottom: -20px;
     margin-left: 25px;
   }
@@ -50,13 +55,13 @@
     width: 2px;
     background-color: white;
     height: 2px;
-    width: 65px;
-    margin-top: -40px;
-    margin-left: 26px;
+    width: 75px;
+    margin-top: -3px;
+    margin-left: 21px;
     transform: rotate(-30deg);
     z-index: -90;
   }
-  .grafo {
+  .grafo-conoce-el-proceso {
     margin: 50px;
     position: absolute;
   }
