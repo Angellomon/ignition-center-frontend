c9b4dd28bd84408e44514d6cd0a923d95b809e98
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:17:28 -0500

    feat(seccion): implementación de sección y grafo de scrum

---
+++ src/lib/components/info/Scrum.svelte
@@ -0,0 +1,24 @@
+<script>
+  import GrafoVerIniciativas from '../grafo/GrafoVerIniciativas.svelte';
+  import ColWrap from './ColWrap.svelte';
+
+  import { seccion } from '../../stores';
+  import GrafoScrum from '../grafo/GrafoScrum.svelte';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<ColWrap>
+  <h3>¿qué es scrum?</h3>
+  <GrafoScrum bind:coords on:enter on:leave animation={$seccion === 'ver_iniciativas'} />
+</ColWrap>
+
+<style>
+  h3 {
+    text-align: center;
+    color: #13213d;
+  }
+</style>
