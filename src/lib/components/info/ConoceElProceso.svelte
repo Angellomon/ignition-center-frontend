199ba75e2ac26b11c390e0f5dbc9225dc88b0b73
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:43:41 -0500

    feat(animacion): implementación de animacion para sección Conoce el PRoceso

--- src/lib/components/info/ConoceElProceso.svelte
+++ src/lib/components/info/ConoceElProceso.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { seccion } from '../../stores';
+
   import GrafoConoceElProceso from '../grafo/GrafoConoceElProceso.svelte';
+  import WrapperAnimacion from '../grafo/WrapperAnimacion.svelte';
   import ColWrap from './ColWrap.svelte';
 
   export let coords = {
@@ -10,7 +13,28 @@
 
 <ColWrap>
   <h3>conoce el proceso</h3>
-  <GrafoConoceElProceso bind:coords on:enter on:leave />
+  <WrapperAnimacion
+    topOffset={8}
+    leftOffset={23}
+    grafoClass=".grafo-conoce-el-proceso"
+    animation={$seccion === 'conoce_el_proceso'}
+    offssetsPosiciones={[
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: 70,
+        y: 0
+      },
+      {
+        x: 0,
+        y: 40
+      }
+    ]}
+  >
+    <GrafoConoceElProceso bind:coords on:enter on:leave />
+  </WrapperAnimacion>
 </ColWrap>
 
 <style>
