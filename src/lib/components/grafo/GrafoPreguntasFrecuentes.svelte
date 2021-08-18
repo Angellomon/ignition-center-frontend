dbb0c90d8783d8fe1037460dad6a1db7b93f0b67
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:17:11 -0500

    faet(secciones): implementación de sección de preguntas frecuentes

---
+++ src/lib/components/grafo/GrafoPreguntasFrecuentes.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Nodo from './Nodo.svelte';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<div class="grafo-preguntas-frecuentes">
+  <span class="nodo-1">
+    <Nodo bind:pos={coords} on:enter on:leave />
+  </span>
+</div>
+
+<style>
+  .nodo-1 {
+    margin-left: 0px;
+    margin-top: -24px;
+  }
+
+  .grafo-preguntas-frecuentes {
+    margin: 50px;
+    position: absolute;
+  }
+</style>
---
+++ src/lib/components/info/PreguntasFrecuentes.svelte
@@ -0,0 +1,28 @@
+<script>
+  import GrafoPreguntasFrecuentes from '../grafo/GrafoPreguntasFrecuentes.svelte';
+  import ColWrap from './ColWrap.svelte';
+
+  import { seccion } from '../../stores';
+
+  export let coords = {
+    x: 0,
+    y: 0
+  };
+</script>
+
+<ColWrap>
+  <h3>preguntas frecuentes</h3>
+  <GrafoPreguntasFrecuentes
+    bind:coords
+    on:enter
+    on:leave
+    animation={$seccion === 'ver_iniciativas'}
+  />
+</ColWrap>
+
+<style>
+  h3 {
+    text-align: center;
+    color: white;
+  }
+</style>
