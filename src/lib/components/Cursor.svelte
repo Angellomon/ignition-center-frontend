73f0780a836a65ddcf85c60a56f2bbec94f8844a
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:39:05 -0500

    feat(cursor): tamaño global en Store

--- src/lib/components/Cursor.svelte
+++ src/lib/components/Cursor.svelte
@@ -1,19 +1,15 @@
 <script>
-  import { spring } from 'svelte/motion';
-  import { seccion } from '../stores';
+  import { cursorSize } from '../stores';
 
   export let x = 0;
   export let y = 0;
-
-  const size = spring(20);
 </script>
 
 <div
-  on:click={() => console.log($seccion)}
-  on:mousedown={() => size.set(35)}
-  on:mouseup={() => size.set(20)}
   class="circle"
-  style={`left: ${x - $size / 2}px; top: ${y - $size / 2}px; height: ${$size}px; width: ${$size}px`}
+  style={`left: ${x - $cursorSize / 2}px; top: ${
+    y - $cursorSize / 2
+  }px; height: ${$cursorSize}px; width: ${$cursorSize}px`}
 />
 
 <style>
