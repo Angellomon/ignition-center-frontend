54f108613ace41c66bcdca1866e50b21c5630337
Angellomon <angelmtzdiaz@gmail.com>
2021-08-18 17:18:40 -0500

    feat(seccion): implementación de sección de inspiración

---
+++ src/lib/components/info/Inspiracion.svelte
@@ -0,0 +1,16 @@
+<script>
+  import ColWrap from './ColWrap.svelte';
+</script>
+
+<ColWrap>
+  <h3>"instead of thinking out of the box, get rid of the box"</h3>
+</ColWrap>
+
+<style>
+  h3 {
+    text-align: center;
+    color: white;
+    align-self: center;
+    margin: 10px 25%;
+  }
+</style>
