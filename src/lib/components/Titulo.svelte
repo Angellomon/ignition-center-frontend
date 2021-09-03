d96f31ce9610343c7bbdbb507fee394462e86fee
Angellomon <angelmtzdiaz@gmail.com>
2021-09-02 20:11:48 -0500

    feat(titulo): prop on y colores

--- src/lib/components/Titulo.svelte
+++ src/lib/components/Titulo.svelte
@@ -1,14 +1,20 @@
 <script>
   import Emphasis from './Emphasis.svelte';
+
+  export let on = false;
 </script>
 
-<h2>igniti<Emphasis>on</Emphasis><br /> center</h2>
+<h2 class:on>igniti<Emphasis>on</Emphasis><br /> center</h2>
 
 <style>
   h2 {
-    color: #75c7ae;
+    color: white;
     font-size: 2.9em;
     text-align: right;
     line-height: 2.7rem;
+  }
+
+  h2.on {
+    color: #75c7ae;
   }
 </style>
